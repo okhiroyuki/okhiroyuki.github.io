697075fef774061233c659f52dd6d5648099a431
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2021-03-28 14:05:20 +0900

    feat: add g-tag

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,23 @@ module.exports = {
   dest: 'docs/.vuepress/dist/',
   theme: 'blog-vuetify',
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico', type: 'image/x-icon' }],
+    [
+      'link', { rel: 'icon', href: '/favicon.ico', type: 'image/x-icon' }
+    ],
+    [
+        'script',
+        {
+            async: true,
+            src: 'https://www.googletagmanager.com/gtag/js?id=G-NPEHXJKD62',
+        },
+    ],
+    [
+        'script',
+        {},
+        [
+            "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-NPEHXJKD62');",
+        ],
+    ],
   ],
   markdown: {
     lineNumbers: true,
